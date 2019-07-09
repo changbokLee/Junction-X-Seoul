569b05de6e526b84bad8326dc19c24a3794ff5e4
changboklee <changbok2453@gmail.com>
2019-07-09 11:59:45 +0900

    장고 서버 로그인폼

---
+++ django_test/myuser/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Myuser(models.Model):
+    username = models.CharField(max_length=128, verbose_name = '아이디') #max length 를 꼭 지정해야함.
+    password = models.CharField(max_length=256, verbose_name = '비밀번호') 
+    register_dttm = models.DateTimeField(auto_now_add=True, verbose_name = '가입일시')
+
+    def _str_(self):
+        return self.username
+
+    class Meta:
+        db_table = 'dls_myuser'
+        verbose_name = '사용자'
+        verbose_name_plural = '사용자'
